32c469f9528ec35346f0d1d32bd78b4598d470c2
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-08-19 17:59:55 +0530

    cors origin added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({origin:['http://localhost:5173'],credentials:true}))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
